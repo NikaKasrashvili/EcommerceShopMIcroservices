ef9eed9e64de3867dcc7ef7a8f39b1f3ddfd2d79
nika <nika.kasro@gmail.com>
2025-10-29 00:53:03 +0400

    Make basket.Api as grpc client for discount grpc server

--- src/Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,12 +1,24 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketresult>;
 public record StoreBasketresult(string UserName);
 
-public class StoreBasketCommandHandler(IBasketRespository _repository) : ICommandHandler<StoreBasketCommand, StoreBasketresult>
+public class StoreBasketCommandHandler(IBasketRespository _repository, DiscountProtoService.DiscountProtoServiceClient _discountProto) 
+    : ICommandHandler<StoreBasketCommand, StoreBasketresult>
 {
     public async Task<StoreBasketresult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+        foreach (var item in command.Cart.Items)
+        {
+            var coupon = await _discountProto.GetDiscountAsync(
+                new GetDiscountRequest { ProductName = item.ProductName },
+                cancellationToken: cancellationToken);
+
+            item.Price -= coupon.Amount;
+        }
+
         await _repository.StoreBasket(command.Cart, cancellationToken);
         return new StoreBasketresult(command.Cart.UserName);
     }
--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,3 +1,4 @@
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -7,12 +8,18 @@ var assembly = typeof(Program).Assembly;
 var connectionString = builder.Configuration.GetConnectionString("Database");
 var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
 
+#region AppServices
+
+builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+#endregion
+
+#region DataServices
 
 builder.Services.AddMarten(opts =>
 {
@@ -20,10 +27,6 @@ builder.Services.AddMarten(opts =>
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName); // make username identity field
 }).UseLightweightSessions();
 
-builder.Services.AddValidatorsFromAssembly(assembly);
-
-builder.Services.AddCarter();
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddScoped<IBasketRespository, BasketRepository>();
 builder.Services.Decorate<IBasketRespository, CachedBasketRepository>(); // using scrutor
 
@@ -31,7 +34,22 @@ builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
+#endregion
 
+#region gRpcServices
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+});
+#endregion
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddHealthChecks()
     .AddRedis(redisConnectionString!)
     .AddNpgSql(connectionString!);
